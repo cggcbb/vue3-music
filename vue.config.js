84f195364a94bca142e2aa2c69f51f93d8b3b53e
eliauk-c <449567533@qq.com>
2021-04-15 11:27:51 +0800

    refactor: 整理初始化代码

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+  productionSourceMap: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/'
+}
