8ef70a0b864e17540cacbf2bfd76292663d8911d
cggcbb <449567533@qq.com>
2021-04-27 14:42:15 +0800

    feat: 渲染歌手列表

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,23 +1,40 @@
 <template>
-  <div>
-    singer
+  <div class="singer">
+    <singer-list :data="singers" v-loading="!singers.length" />
   </div>
 </template>
 
 <script>
 import { getSingerList } from '@/service/singer'
+import SingerList from '@/components/singer-list/singer-list'
+import { ref, onBeforeMount } from 'vue'
+
 export default {
   name: 'singer',
-  data() {
+  setup() {
+    const singers = ref([])
+
+    onBeforeMount(async () => {
+      // 获取歌手列表数据
+      const result = await getSingerList()
+      singers.value = result.singers
+    })
+
     return {
-      singers: []
+      singers
     }
   },
-  async created() {
-    const result = await getSingerList()
-    console.log(result)
+  components: {
+    SingerList
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
